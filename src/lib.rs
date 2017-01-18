c098e6b6a0d102a32fe3b313c1cea963fce1681a
BookOwl <stanleybookowl@gmail.com>
2017-01-18 09:59:35 -0500

    Add Event, Key, and Cursor

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,38 @@
+pub mod event {
+    #[derive(PartialEq, Debug, Copy, Clone)]
+    pub enum Event {
+        Keypress(Key),
+    }
+
+    #[derive(PartialEq, Debug, Copy, Clone)]
+    pub enum Key {
+        Up,
+        Down,
+        Left,
+        Right,
+        Control,
+        Alt,
+        Tab,
+        Esc,
+        Delete,
+        Enter,
+        Char(char),
+    }
+}
+
+pub mod cursor {
+    pub struct Cursor {
+        pub line: i64,
+        pub column: i64,
+    }
+
+    impl Cursor {
+        pub fn new(line: i64, column: i64) -> Cursor {
+            Cursor { line: line, column: column }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
